e376e9512c58328949b387933ee0a00a1561157f
Petr Tripolsky <tripolskypetr@gmail.com>
2025-01-24 02:39:28 +0500

    patch

--- src/functions/changeAgent.ts
+++ src/functions/changeAgent.ts
@@ -2,6 +2,7 @@ import { AgentName } from "../interfaces/Agent.interface";
 import { SwarmName } from "../interfaces/Swarm.interface";
 import swarm from "../lib";
 
-export const changeAgent = async (agentName: AgentName, clientId: string, swarmName: SwarmName) => {
+export const changeAgent = async (agentName: AgentName, clientId: string) => {
+    const swarmName = swarm.sessionValidationService.getSwarm(clientId);
     await swarm.swarmPublicService.setAgentName(agentName, clientId, swarmName);
 };
