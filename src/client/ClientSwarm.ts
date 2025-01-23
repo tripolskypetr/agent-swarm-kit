abd9caa5f9122ffaec1328c7a9ea237cb6a81329
Petr Tripolsky <tripolskypetr@gmail.com>
2025-01-24 00:49:25 +0500

    patch

--- src/client/ClientSwarm.ts
+++ src/client/ClientSwarm.ts
@@ -4,8 +4,7 @@ import { AgentName, IAgent } from "src/interfaces/Agent.interface";
 import ISwarm, { ISwarmParams } from "src/interfaces/Swarm.interface";
 
 export class ClientSwarm implements ISwarm {
-  
-  private _activeAgent: AgentName;  
+  private _activeAgent: AgentName;
 
   constructor(readonly params: ISwarmParams) {
     this._activeAgent = params.defaultAgent;
@@ -13,37 +12,40 @@ export class ClientSwarm implements ISwarm {
 
   waitForOutput = async (): Promise<string> => {
     this.params.logger.debug("BaseConnection waitForOutput");
-    for (let i = 0; i !== GLOBAL_CONFIG.CC_ANSWER_TIMEOUT_SECONDS; i++) {
+    const START_TIME = Date.now();
+    while (true) {
+      const elapsedTime = Date.now() - START_TIME;
+      if (elapsedTime >= GLOBAL_CONFIG.CC_ANSWER_TIMEOUT_SECONDS) {
+        throw new Error(
+          `agent-swarm ClientSwarm waitForOutput timeout reached for ${this.params.swarmName}`
+        );
+      }
       const [agentName, output] = await Promise.race(
-        Object.entries(this.params.agentMap).map(async ([agentName, agent]) => [agentName, await agent.waitForOutput()])
+        Object.entries(this.params.agentMap).map(async ([agentName, agent]) => [
+          agentName,
+          await agent.waitForOutput(),
+        ])
       );
-      if (agentName === await this.getAgentName()) {
+      if (agentName === (await this.getAgentName())) {
         return output;
       }
       await sleep(1_000);
     }
-    throw new Error(`agent-swarm ClientSwarm waitForOutput timeout reach for ${this.params.swarmName}`);
   };
 
   getAgentName = async (): Promise<AgentName> => {
-    this.params.logger.debug(
-      `ClientSwarm getAgentName`
-    );
+    this.params.logger.debug(`ClientSwarm getAgentName`);
     return this._activeAgent;
   };
 
   getAgent = async () => {
-    this.params.logger.debug(
-      `ClientSwarm getAgent`
-    );
+    this.params.logger.debug(`ClientSwarm getAgent`);
     const agent = await this.getAgentName();
     return this.params.agentMap[agent];
   };
 
   setAgentName = async (agentName: AgentName) => {
-    this.params.logger.debug(
-      `ClientSwarm setAgentName agentName=${agentName}`
-    );
+    this.params.logger.debug(`ClientSwarm setAgentName agentName=${agentName}`);
     this._activeAgent = agentName;
   };
 }
