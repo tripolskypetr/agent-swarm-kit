108e78009f7613ca2501f1ba473db7c8ef3392ac
Petr Tripolsky <tripolskypetr@gmail.com>
2025-02-14 21:41:11 +0500

    patch

--- src/utils/removeXmlTags.ts
+++ src/utils/removeXmlTags.ts
@@ -1,5 +1,9 @@
 export const removeXmlTags = (input: string) => {
-    return input.replace(/<[^>]*>.*?<\/[^>]*>/g, '');
+    return input
+        .replace(/<[^>]+>[\s\S]*?<\/[^>]+>/g, '')
+        .replace(/\n\s*\n/g, '\n')
+        .trim();
 };
+
 
 export default removeXmlTags;
