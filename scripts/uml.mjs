145624e4e7ef8ba5e24c28e787d9c5bd645a67ec
Petr Tripolsky <tripolskypetr@gmail.com>
2025-02-28 16:12:49 +0500

    uml-schema

---
+++ scripts/uml.mjs
@@ -0,0 +1,46 @@
+import { trycatch } from "functools-kit";
+import { writeFile } from "fs/promises";
+import * as moduleData from "../build/index.mjs";
+
+{
+    Object.assign(globalThis, moduleData);
+}
+
+const UML_STEP = '\t';
+const UML_BULLET = "•";
+const MAX_NESTING = 3;
+
+const toUML = async () => {
+
+    const getNodes = (targetObj) =>
+        Object.getOwnPropertyNames(targetObj)
+            .filter(trycatch((propertyName) => targetObj[propertyName].name && propertyName.endsWith("Service"), { defaultValue: false }))
+
+    const lines = [];
+    const process = (entry, keys, level = 0) => {
+        for (const key of keys) {
+            const targetObj = Object.getPrototypeOf(entry[key]);
+            const nodes = getNodes(targetObj);
+            const space = [...new Array(level)].fill(UML_STEP).join('');
+            if (nodes.length && level < MAX_NESTING) {
+                lines.push(`${space}${String(key)}:`);
+                lines.push(`${space}${UML_STEP}${UML_BULLET} ${String(key)}: ""`);
+                process(targetObj, nodes, level + 1);
+            } else {
+                lines.push(`${space}${String(key)}: ""`);
+            }
+        }
+    };
+    process(swarm, Object.keys(swarm))
+
+    const result = ['@startyaml', ...lines, '@endyaml'].join('\n');
+
+    await writeFile("./docs/uml.puml", result);
+
+};
+
+ExecutionContextService.runInContext(() => {
+    MethodContextService.runInContext(() => {
+        toUML(swarm)
+    }, {})
+}, {});
