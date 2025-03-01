4cd413831fb3016d3a72af837ceeb23aaeae4286
Petr Tripolsky <tripolskypetr@gmail.com>
2025-03-01 19:43:01 +0500

    rollup-terser-cfg

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,6 +1,7 @@
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import typescript from "@rollup/plugin-typescript";
 import { dts } from "rollup-plugin-dts";
+import terser from "@rollup/plugin-terser";
 import path from "path";
 
 export default [
@@ -9,10 +10,12 @@ export default [
     output: [
       {
         file: path.join("build", "index.mjs"),
+        sourcemap: true,
         format: "esm",
       },
       {
         file: path.join("build", "index.cjs"),
+        sourcemap: true,
         format: "commonjs",
       },
     ],
@@ -23,6 +26,16 @@ export default [
       typescript({
         tsconfig: "./tsconfig.json",
         noEmit: true,
+      }),
+      terser({
+        ecma: 2020,
+        compress: {
+          drop_console: false,
+        },
+        format: {
+          comments: false,
+        },
+        sourceMap: true,
       }),
     ],
   },
