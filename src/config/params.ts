9be9673a6772cda251b715ee85ddfe6740663668
Petr Tripolsky <tripolskypetr@gmail.com>
2025-03-12 14:59:57 +0500

    cleanup

--- src/config/params.ts
+++ src/config/params.ts
@@ -172,7 +172,7 @@ const GLOBAL_CONFIG = {
   CC_TOOL_CALL_EXCEPTION_CUSTOM_FUNCTION,
 };
 
-GLOBAL_CONFIG.CC_RESQUE_STRATEGY = "recomplete";
+GLOBAL_CONFIG.CC_RESQUE_STRATEGY = "flush";
 
 export const setConfig = (config: Partial<typeof GLOBAL_CONFIG>) => {
   Object.assign(GLOBAL_CONFIG, config);
