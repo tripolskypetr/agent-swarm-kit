520e6f036868d01beda61f506ff36713ddde52fe
Petr Tripolsky <tripolskypetr@gmail.com>
2025-03-18 14:09:14 +0500

    model-message-payload

--- src/functions/common/getPayload.ts
+++ src/functions/common/getPayload.ts
@@ -9,17 +9,18 @@ const METHOD_NAME = "function.common.getPayload";
  * Retrieves the payload from the current PayloadContextService context.
  * Returns null if no context is available. Logs the operation if logging is enabled.
  * @template Payload - The type of the payload object, defaults to a generic object.
- * @returns A promise resolving to the payload data from the context, or null if no context exists.
+ * @returns the payload data from the context, or null if no context exists.
  * @example
  * const payload = await getPayload<{ id: number }>();
- * console.log(payload); // { id: number } or {}
+ * console.log(payload); // { id: number } or null
  */
-export const getPayload = async <
+export const getPayload = <
   Payload extends object = object
->(): Promise<Payload | null> => {
+>(): Payload | null => {
   GLOBAL_CONFIG.CC_LOGGER_ENABLE_LOG && swarm.loggerService.log(METHOD_NAME);
-  const result = PayloadContextService.hasContext()
-    ? swarm.payloadContextService.context as Payload
-    : null;
-  return result;
+  if (PayloadContextService.hasContext()) {
+    const { payload } = swarm.payloadContextService.context;
+    return payload as Payload;
+  }
+  return null;
 };
