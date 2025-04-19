61dd63ced7e55593152ef141257403ff7605fe31
Petr Tripolsky <tripolskypetr@gmail.com>
2025-04-19 22:19:32 +0500

    docs

--- demo/mcp-weather-chat/src/lib/swarm.ts
+++ demo/mcp-weather-chat/src/lib/swarm.ts
@@ -20,8 +20,17 @@ export const TEST_MCP = addMCP({
       arguments: params,
     });
     const [{ text }] = content as any;
-    await commitToolOutput(toolId, text, clientId, agentName);
-    await execute("", clientId, agentName);
+    /** 
+     * 
+     * Can simple return the text output to execute model automically,
+     * If you want to emit event, just return undefined, custom code allowed here
+     * 
+     * @example
+     * await commitToolOutput(toolId, text, clientId, agentName);
+     * await execute("", clientId, agentName);
+     * 
+     */
+    return text;
   },
 })
 
